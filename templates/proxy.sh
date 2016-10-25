d712a206b3900b0cafd5e6dd6a3d9b742333e6e4
Nels Dachel <ndachel@gmail.com>
2016-10-25 10:39:59 -0600

    docker.socket

--- templates/proxy.sh
+++ templates/proxy.sh
@@ -2,4 +2,4 @@ export HTTP_PROXY=http://web-proxy.corp.hpecorp.net:8080
 export http_proxy=http://web-proxy.corp.hpecorp.net:8080
 export HTTPS_PROXY=http://web-proxy.corp.hpecorp.net:8080
 export https_proxy=http://web-proxy.corp.hpecorp.net:8080
-export no_proxy="127.0.0.1,localhost,172.16.1.16,172.16.1.17,172.16.1.18,172.16.1.21,172.16.1.20,172.16.1.24,172.16.1.23,172.16.1.22,172.16.1.25,172.16.1.70,172.16.1.71,dcos-master-1,dcos-master-2,dcos-master-3,dcos-agent-1,dcos-agent-2,dcos-agent-3,dcos-agent-4,dcos-agent-5,dcos-agent-6,dcos-agent-7,dcos-monitor,marathon.mesos, etcd.marathon.mesos"
+export no_proxy="127.0.0.1,localhost,172.16.1.15,172.16.1.16,172.16.1.17,172.16.1.18,172.16.1.21,172.16.1.20,172.16.1.24,172.16.1.23,172.16.1.22,172.16.1.25,172.16.1.70,172.16.1.71,dcos-master-1,dcos-master-2,dcos-master-3,dcos-agent-1,dcos-agent-2,dcos-agent-3,dcos-agent-4,dcos-agent-5,dcos-agent-6,dcos-agent-7,dcos-monitor,marathon.mesos, etcd.marathon.mesos"
